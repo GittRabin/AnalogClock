0857e1776b0a99ebba22f2b2598b900d10959b68
Rabin Chaudhary <nibar18@gmail.com>
2018-05-30 16:09:19 +1000

    Define custom postions

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+
+let hrPosition = 29;
+let minPosition = 90;
+let secPosition = 40;
+
+
+HOURHAND.style.transform="rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform="rotate("+ minPosition+"deg)";
